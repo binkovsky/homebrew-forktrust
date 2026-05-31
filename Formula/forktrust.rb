08d5b7bb850a84ceeaafd9a62d087a8b28f5928f
binkovsky <binkovsky.work@gmail.com>
2026-05-31 15:16:19 +0300

    bump: forktrust 0.5.0 (exec + status + edit prediction)

--- Formula/forktrust.rb
+++ Formula/forktrust.rb
@@ -1,28 +1,28 @@
 class Forktrust < Formula
   desc "Safe-by-default git worktree manager for parallel AI coding agents"
   homepage "https://github.com/binkovsky/forktrust"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.4.0/forktrust_0.4.0_darwin_arm64.tar.gz"
-      sha256 "e011f554b4d6d693fae13231249d47ab68f559c4cb06c50a87eab921b1510d83"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.5.0/forktrust_0.5.0_darwin_arm64.tar.gz"
+      sha256 "a036896bd81bd79ad7fcf56892b28aac15e2df8e949add0898d61e708024d397"
     end
     on_intel do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.4.0/forktrust_0.4.0_darwin_amd64.tar.gz"
-      sha256 "35619721f2b85b09a64a01e490fbf66293fe3ad94e74e7c1a63eeabf4ee18ed0"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.5.0/forktrust_0.5.0_darwin_amd64.tar.gz"
+      sha256 "e45ad12c10fe405a3fc6fc943e2f95bb13a26422e8a4119726c24aa8d481976e"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.4.0/forktrust_0.4.0_linux_arm64.tar.gz"
-      sha256 "c368e23657047f2466818c764f6e24a0913a375f027fce1560ddaefb8b499a55"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.5.0/forktrust_0.5.0_linux_arm64.tar.gz"
+      sha256 "73a5d92660f3c66eae53df80c75ed02ffe0296e4a682d41ab2b2acccfb2fcd75"
     end
     on_intel do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.4.0/forktrust_0.4.0_linux_amd64.tar.gz"
-      sha256 "b74caad3c254c6282975e42234d13751d6b90891c7ef44354a2310a9ec9e5b11"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.5.0/forktrust_0.5.0_linux_amd64.tar.gz"
+      sha256 "ecc164f303df17e770aea3198ffb13d81daadc9dc2da061243cc2b4c09db6a9c"
     end
   end
 
