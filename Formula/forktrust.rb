f4cacaed0a21365822d426cbe09dac59be0ec69f
binkovsky <binkovsky.work@gmail.com>
2026-05-31 19:32:26 +0300

    bump: forktrust 0.6.1 (symlink confinement + dry-run accuracy)

--- Formula/forktrust.rb
+++ Formula/forktrust.rb
@@ -1,28 +1,28 @@
 class Forktrust < Formula
   desc "Safe-by-default git worktree manager for parallel AI coding agents"
   homepage "https://github.com/binkovsky/forktrust"
-  version "0.6.0"
+  version "0.6.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.0/forktrust_0.6.0_darwin_arm64.tar.gz"
-      sha256 "9d6699441067403dfef7472b2874cdda874569891d396eed2fde05568c570b8f"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.1/forktrust_0.6.1_darwin_arm64.tar.gz"
+      sha256 "222fa5eae43973758552394294f11099fc3f9756a198ad43a77f0fe0f24bdee1"
     end
     on_intel do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.0/forktrust_0.6.0_darwin_amd64.tar.gz"
-      sha256 "02d51efdd711b88536949f27c66ca01e0ce9eb4f7c02d14a8f42b78c74637cdc"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.1/forktrust_0.6.1_darwin_amd64.tar.gz"
+      sha256 "c5f827e7cc3189c7f3102c53e6d2adc08b213abe17f1919359a87d51a4653134"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.0/forktrust_0.6.0_linux_arm64.tar.gz"
-      sha256 "6af095770bd29c15b3f7d9a65a82fe5a6ae7627370ac4767c8adecd6e3728854"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.1/forktrust_0.6.1_linux_arm64.tar.gz"
+      sha256 "f079252f2638cadf6b701b664c66b69897f038ff52867841438b58d29ed5f98d"
     end
     on_intel do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.0/forktrust_0.6.0_linux_amd64.tar.gz"
-      sha256 "bbe86a8c5b2e697ad5fb8c524e3e4fb7e861dfdbb699ca296886a76f3d6ee5c4"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.1/forktrust_0.6.1_linux_amd64.tar.gz"
+      sha256 "239e39a0329f92b285d1679b7fe47df2fa0039c21160c0d59f60f78f436121b6"
     end
   end
 
