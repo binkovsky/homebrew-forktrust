164f9465c7b89dfe51302bafb9fc5f247b79c967
binkovsky <binkovsky.work@gmail.com>
2026-06-01 10:21:28 +0300

    forktrust 0.6.5

--- Formula/forktrust.rb
+++ Formula/forktrust.rb
@@ -1,28 +1,28 @@
 class Forktrust < Formula
   desc "Safe-by-default git worktree manager for parallel AI coding agents"
   homepage "https://github.com/binkovsky/forktrust"
-  version "0.6.4"
+  version "0.6.5"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.4/forktrust_0.6.4_darwin_arm64.tar.gz"
-      sha256 "8264ae2fe6aab5a9451d5acc3110c3c562092b46505ba49d4206414d2a1bc2a0"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.5/forktrust_0.6.5_darwin_arm64.tar.gz"
+      sha256 "51c45a50de7b7040e98ad2bcab00a97c9f960bed3d37cb3a8f71273a271ef7da"
     end
     on_intel do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.4/forktrust_0.6.4_darwin_amd64.tar.gz"
-      sha256 "8ef489f51cd5ed8d582a6e911f70ef738e5034edc71970219df8248dace97bf1"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.5/forktrust_0.6.5_darwin_amd64.tar.gz"
+      sha256 "c5765813cc1285072aee111c87429263cc04b3e03322a2badec409891a6f417e"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.4/forktrust_0.6.4_linux_arm64.tar.gz"
-      sha256 "8aab8ceda6cd17a03a81a066783751551027f9f3dae70b589e7402029c3f99de"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.5/forktrust_0.6.5_linux_arm64.tar.gz"
+      sha256 "3565ab5a93ef88f80a606a83b06db0e1a02b4274f00476faa1683b08ba86ca98"
     end
     on_intel do
-      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.4/forktrust_0.6.4_linux_amd64.tar.gz"
-      sha256 "e053145537b818b894c321790d77d5f6583dca950c95faa36f6a3c4b69a4ed1e"
+      url "https://github.com/binkovsky/forktrust/releases/download/v0.6.5/forktrust_0.6.5_linux_amd64.tar.gz"
+      sha256 "68c4c099b678e5c07e084407adb4ddb9ea4644147431be0728e1c78f837819ce"
     end
   end
 
